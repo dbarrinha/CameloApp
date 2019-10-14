077c7024dbbf71b3ffe956bbbbde8ea24264d50a
unknown <dbarrinha@hotmail.com>
2019-10-14 18:37:15 -0300

    favoritos

--- App.js
+++ App.js
@@ -25,7 +25,7 @@ class AuthLoadingScreen extends React.Component {
   _bootstrapAsync = async () => {
     const user = await AsyncStorage.getItem('user:CameloApp')
     if (user === null) {
-      this.props.navigation.navigate('AuthStack')
+      this.props.navigation.navigate('HomeStack')
     } else {
       console.log("teste")
     }
@@ -41,8 +41,8 @@ class AuthLoadingScreen extends React.Component {
 }
 
 const HomeStack = createNativeStackNavigator({
-  Home: Home,
   Details: Details,
+  Home: Home,
 },
   {
     headerMode: 'none',
---
+++ src/screens/Favoritos/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { View, Text, Dimensions, Image, StyleSheet } from 'react-native';
+import { colors } from 'config/colors';
+
+// import { Container } from './styles';
+
+export default function Favoritos() {
+    return (
+        <View style={{ flex: 1, backgroundColor: colors.colorBackground, paddingVertical: 5 }} >
+
+        </View>
+  );
+}
